193a268ca3ea94cda3d092343191b81de0f6b585
Balcihatice <aciceaybike@gmail.com>
2023-03-18 16:30:31 +0300

    B103_API

--- src/test/java/testData/HerokuAppTestData.java
+++ src/test/java/testData/HerokuAppTestData.java
@@ -1,8 +1,24 @@
 package testData;
-
-import io.restassured.specification.RequestSpecification;
+import java.util.HashMap;
+import java.util.Map;
 
 public class HerokuAppTestData {
+    public Map<String, String> bookingdatesMapMethod(String checkin, String checkout){
+        Map<String, String> bookingdatesMap = new HashMap<>();
+        bookingdatesMap.put("checkin",checkin);
+        bookingdatesMap.put("checkout",checkout);
+        return bookingdatesMap;
+    }
+    public Map<String, Object> expectedDataMethod(String firstname,String lastname, Integer totalprice, Boolean depositpaid, Map<String, String>  bookingdatesMap, String additionalneeds ){
+        Map<String, Object> expectedData = new HashMap<>();
+        expectedData.put("firstname",firstname);
+        expectedData.put("lastname",lastname);
+        expectedData.put("totalprice",totalprice);
+        expectedData.put("depositpaid",depositpaid);
+        expectedData.put("bookingdates",bookingdatesMap);
+        expectedData.put("additionalneeds",additionalneeds);
 
+        return expectedData;
+    }
 
 }
