e02d67bdb5fa2faab0098b18048edea1dd3336bf
Balcihatice <aciceaybike@gmail.com>
2023-03-19 23:00:18 +0300

    B103_API

---
+++ src/test/java/homework/H04.java
@@ -0,0 +1,41 @@
+package homework;
+
+import base_urls.HerOkuAppBaseUrl;
+import io.restassured.response.Response;
+import org.junit.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class H04 extends HerOkuAppBaseUrl {
+
+     /*
+        Given
+            https://restful-booker.herokuapp.com/booking?firstname=Josh&lastname=Allen  ==> Data değişebilir
+        When
+            User sends get request to the URL
+        Then
+            Status code is 200
+	  	And
+	  		Among the data there should be someone whose firstname is "Josh" and lastname is "Allen"   ==> Data değişebilir
+
+     */
+
+    @Test
+    public void get04(){
+//Set the Url
+        spec.pathParam("first","booking").queryParams("firstname","Josh","lastname","Allen");
+
+//Set The Expected Data
+
+//Send The Request and Get The Response
+        Response response = given().spec(spec).when().get("/{first}");
+        response.prettyPrint();
+
+//Do Assertion
+        assertEquals(200,response.statusCode());
+        assertTrue(response.asString().contains("bookingid"));
+
+    }
+}
